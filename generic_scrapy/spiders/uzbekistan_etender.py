bce120445f470585472310a0082de4b5741f5051
Yohanna Lisnichuk <yohanitalisnichuk@gmail.com>
2023-12-01 14:01:16 -0300

    feat(uzbekistan_etender): update endpoint to use mandatory filter system_id

--- generic_scrapy/spiders/uzbekistan_etender.py
+++ generic_scrapy/spiders/uzbekistan_etender.py
@@ -32,7 +32,7 @@ class UzbekistanEtender(UzbekistanBaseSpider):
         for item in response.json():
             yield item
             yield scrapy.Request(
-                f"https://apietender.uzex.uz/api/common/GetTrade/{item['trade_id']}",
+                f"https://apietender.uzex.uz/api/common/GetTrade/{item['trade_id']}/0",
                 callback=self.parse_trade,
             )
 
@@ -46,6 +46,7 @@ class UzbekistanEtender(UzbekistanBaseSpider):
             "From": from_parameter,
             "To": to_parameter,
             "DeadlineStart": self.from_date.strftime("%Y-%m-%dT%H:%M:%S.000Z"),
+            "System_Id": 0,
         }
         if self.until_date:
             filters["DeadlineEnd"] = self.until_date.strftime("%Y-%m-%dT%H:%M:%S.000Z")
